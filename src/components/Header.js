b6e1da641b3e578dfb9c57a13b9a8c18bff3f7f5
bluerhythmz <73447805+bluerhythmz@users.noreply.github.com>
2021-08-25 17:42:56 -0700

    finished mobile ui - need to fix image loading

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,55 @@
 import React from 'react'
 import Burger from '../assets/icon-hamburger.svg'
 import Planets from '../data.json'
+import { NavLink, Route } from 'react-router-dom'
+import Arrow from '../assets/icon-chevron.svg'
+import { useState } from 'react'
+import Planet from './Planet'
+
 
 const Header = () => {
-    console.log(Planets[0].name)
+    const [clicked, setClicked] = useState(false)
+    const handleClick = () => {
+        setClicked(!clicked)
+    }
     return (
+        <>
         <header className="header">
             <div className="header__title-box">
                 <h1 className="header__title">The Planets</h1>
-                <img src={Burger} alt="" className="header__burger" />
+                <img src={Burger} alt="" onClick={handleClick} className="header__burger" />
             </div>
-            <nav className="nav">
-                <ul>
+            <nav className="nav" style={clicked ? {display: "block"} : {display: "none"}}>
+                <ul className="nav__links">
                     {Planets.map(planet => (
-                        <li>{planet.name}</li>
+                        <li key={planet.name} className="nav__li">
+                            <NavLink to={{
+                                pathname:'/',
+                                planetProps: {
+                                    planet: planet.name,
+                                    images: planet.images,
+                                    text: {
+                                        overview: planet.overview,
+                                        structure: planet.structure,
+                                        geology: planet.geology
+                                    },
+                                    data: {
+                                        rotation: planet.rotation,
+                                        revolution: planet.revolution,
+                                        radius: planet.radius,
+                                        temperature: planet.temperature
+                                    }
+                                }
+                            }}
+                              className="nav__link">{planet.name}</NavLink>
+                            <img src={Arrow} alt="" className="nav__arrow" />
+                        </li>
                     ))}
                 </ul>
             </nav>
         </header>
+            <Route path="/" component={Planet} />
+        </>
     )
 }
 
--- src/components/Planet.js
+++ src/components/Planet.js
@@ -1,25 +1,47 @@
 import React from 'react'
 import Button from '../components/Button'
-import Mercury from '../assets/planet-mercury.svg'
 import SourceIcon from '../assets/icon-source.svg'
+import { useEffect, useState } from 'react'
+import MercuryPlanet from '../assets/planet-mercury.svg'
 
-const Planet = () => {
+const Planet = ({location}) => {
+    const [planet, setPlanet] = useState('')
+    const [data, setData] = useState('')
+    const [images, setImages] = useState('')
+    const [text, setText] = useState('')
+    
+    useEffect(() => {
+        /* const imagesArr = [{
+            planet: "Mercury",
+            images: {
+                planet: MercuryPlanet
+            }
+        }] */
+        if (location !== undefined && location.planetProps !== undefined) {
+            setPlanet(location.planetProps.planet)
+            setData(location.planetProps.data)
+            setImages(location.planetProps.images.planet)
+            setText(location.planetProps.text)
+        } 
+       
+    }, [location])
     return (
-        <main className="grid">
+        <>
+        {planet && <main className="grid">
             <div className="info-tabs">
                 <Button title="Overview" />
                 <Button title="Structure" />
                 <Button title="Surface" />
             </div>
             <div className="img-wrapper">
-                <img src={Mercury} alt="" className="img" />
+                <img src={images} alt="" className="img" />
             </div>
             <div className="text">
-                <h2 className="text__title">Mercury</h2>
-                <div className="text__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea corrupti commodi totam atque eveniet eaque distinctio veniam sequi neque nihil dolorum quibusdam vero consequuntur corporis quia, cumque, voluptas labore quaerat!</div>
+                <h2 className="text__title">{planet}</h2>
+                <div className="text__description">{text.overview.content}</div>
                 <span className="text__source">
                     Source: 
-                     <a href="./" className="source">
+                     <a href={text.overview.source} className="source">
                           Wikipedia
                          <img src={SourceIcon} alt="link-icon" className="source__img"/>
                     </a>
@@ -28,22 +50,24 @@ const Planet = () => {
             <div className="data">
                 <div className="data__item">
                     <div className="data__title">Rotation Time</div>
-                    <div className="data__stat">830773</div>
+                    <div className="data__stat">{data.rotation}</div>
                 </div>
                 <div className="data__item">
-                    <div className="data__title">Rotation Time</div>
-                    <div className="data__stat">830773</div>
+                    <div className="data__title">Revolution Time</div>
+                    <div className="data__stat">{data.revolution}</div>
                 </div>
                 <div className="data__item">
-                    <div className="data__title">Rotation Time</div>
-                    <div className="data__stat">830773</div>
+                    <div className="data__title">Radius</div>
+                    <div className="data__stat">{data.radius}</div>
                 </div>
                 <div className="data__item">
-                    <div className="data__title">Rotation Time</div>
-                    <div className="data__stat">830773</div>
+                    <div className="data__title">Average Temp</div>
+                    <div className="data__stat">{data.temperature}</div>
                 </div>
             </div>
         </main>
+        }
+        </>
     )
 }
 
